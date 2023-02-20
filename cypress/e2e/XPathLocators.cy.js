daa60e9e78249c90921abcce3033c669f01bbe9c
Kabindra Rijal <kabindra.rijal@sevadev.com>
2023-02-20 08:15:44 +0545

    Use xpath

---
+++ cypress/e2e/XPathLocators.cy.js
@@ -0,0 +1,32 @@
+// XPath locators
+// cy.xpath(Xpath)
+describe('XPath Locators', () => {
+    it('Find numbers of cards', () => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+        cy.get("[name='username']").type("Admin") //used attribute
+
+        cy.get(".oxd-input[name='password']").type("admin123") //using class and attribute
+        cy.get(".oxd-button").click() //used class
+
+        cy.get(".oxd-topbar-header-title").contains("Dashboard") //Assertion
+
+        //  Note: .get is only used for selecting CSS
+
+        cy.xpath("//ul[@class='oxd-main-menu']/li").should('have.length', 11) //using XPath
+    })
+
+    it('Chained xpath',()=>{
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+        cy.get("[name='username']").type("Admin") //used attribute
+
+        cy.get(".oxd-input[name='password']").type("admin123") //using class and attribute
+        cy.get(".oxd-button").click() //used class
+
+        cy.get(".oxd-topbar-header-title").contains("Dashboard") //Assertion
+
+        //  Note: .get is only used for selecting CSS
+        cy.xpath("//ul[@class='oxd-main-menu']").xpath("./li").should('have.length', 11) //using XPath
+    })
+})
